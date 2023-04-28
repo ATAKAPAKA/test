54593dc114ff3a96489978ec096c6c9f044602f9
ATAKAPAKA <73017021+ATAKAPAKA@users.noreply.github.com>
2023-04-28 16:10:02 +0300

    Add files via upload

--- view/main.php
+++ view/main.php
@@ -1,11 +1,11 @@
-<label>Поиск по зонам:<input type="text" id="zone-search"></label>
-
-<ul id="zone-list">
-</ul>
-
-<div id="selected-zones">
-    <h3>Выбранные зоны:</h3>
-    <ul id="selected-zones-list">
+<body>
+    <label>Поиск по зонам:<input type="text" id="zone-search"></label>
+    <ul id="zone-list">
     </ul>
-</div>
-<script src="js.js"></script>
+    <div id="selected-zones">
+        <h3>Выбранные зоны:</h3>
+        <ul id="selected-zones-list">
+        </ul>
+    </div>
+    <script src="js.js"></script>
+</body>
